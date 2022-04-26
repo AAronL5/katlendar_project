cb0e811fe07178a5ec6a6338e55eeaf255d3c9d8
AAronL5 <94020879+AAronL5@users.noreply.github.com>
2022-04-26 12:32:30 -0500

    master

--- Screens/KatlendarCalendar.js
+++ Screens/KatlendarCalendar.js
@@ -24,7 +24,7 @@ const KatlendarCalendar = ({ navigation }) => {
         textDisabledColor: "#d9e1e8",
         dotColor: "#00adf5",
         selectedDotColor: "#ffffff",
-        arrowColor: "blue",
+        arrowColor: "orange",
         monthTextColor: "blue",
         indicatorColor: "blue",
         textDayFontFamily: "Arial",
