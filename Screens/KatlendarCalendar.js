c862c4e9764dd1eb3416ed9694144792cd55147b
ericBuckner5 <104389891+ericBuckner5@users.noreply.github.com>
2022-04-28 19:16:14 -0500

    Updated version of the calender 2.0

--- Screens/KatlendarCalendar.js
+++ Screens/KatlendarCalendar.js
@@ -12,38 +12,75 @@ let { width } = Dimensions.get('window');
 const KatlendarCalendar = ({ navigation }) => {
   const [events, setEvents] = useState([
     {
+      start: '2022-04-29 06:30:00',
+      end: '2022-04-29 09:00:00',
+      title: 'SHSU Vs Lamar',
+      summary: 'Baseball @ Elliot T. Bowers Stadium',
+      
+    },
+    {
+      start: '2022-04-30 09:00:00',
+      end: '2022-04-30 11:00:00',
+      title: 'TAFE Color Run',
+      summary: '5K color run @ Garrett Teacher Education Center',
+      
+    },
+    {
       start: '2022-05-01 01:00:00',
       end: '2022-05-01 03:00:00',
       title: 'SHSU Vs Lamar',
-      summary: 'Baseball',
-      summary: 'Elliot T. Bowers Stadium',
+      summary: 'Baseball @ Elliot T. Bowers Stadium',
+      
     },
     {
       start: '2022-05-01 03:00:00',
       end: '2022-05-01 04:00:00',
       title: 'Sweat the Stress',
-      summary: 'Join the SHSU Rec Sports for a week of activity to de-stress during finals.',
+      summary: 'Join the SHSU Rec Sports for a week of activity to de-stress during finals @ Recreation Sports Center.',
     },
-    {
-      start: '2022-05-01 01:00:00',
-      end: '2022-05-01 02:30:00',
-      title: 'Waterfall Kayaking with Outdoor Recreation',
-      summary: 'Lets go chasing waterfalls! Join us on a journey up Rocky Creek',
-      summary: 'off of Lake Livingston to find this hidden geological gem. Transportation, lunch, and gear.'
-    },
+    
     {
       start: '2022-05-02 07:30:00',
       end: '2022-05-02 09:30:00',
       title: 'SHSU Harp Ensemble: Harp Impressions',
-      summary: 'Each semester, the School of Music hosts recitals to celebrate the work of musicians and provide valuable experience for students to work in excel',
+      summary: 'Each semester, the School of Music hosts recitals to celebrate the work of musicians and provide valuable experience for students to work in excel, @ Gaertner Performing Arts Center',
     },
     {
-      start: '2022-05-01 01:30:00',
-      end: '2022-05-01 01:50:00',
-      title: 'SHSU Vs Lamar',
-      summary: 'Baseball',
-      summary: 'Elliot T. Bowers Stadium'
+      start: '2022-05-03 06:45:00',
+      end: '2022-05-03 07:50:00',
+      title: 'Group Cycle with Rec Sports',
+      summary: 'Indoor cycling class, @ Recreational Sports Center (MP2)',
+      
     },
+    {
+      start: '2022-05-04 01:00:00',
+      end: '2022-05-04 02:30:00',
+      title: 'Waterfall Kayaking with Outdoor Recreation',
+      summary: 'Lets go chasing waterfalls! Join us on a journey up Rocky Creek @ Lake Livingston',
+     
+    },
+    {
+      start: '2022-05-05 03:00:00',
+      end: '2022-05-05 04:30:00',
+      title: 'RULE',
+      summary: 'Research as an Undergraduate Learning Experience @ Garrett Teacher Education Center, 279',
+      
+    },
+    {
+      start: '2022-05-06 06:30:00',
+      end: '2022-05-06 09:00:00',
+      title: 'SHSU vs SFA',
+      summary: 'Baseball @ Elliot T. Bowers Stadium',
+
+    },
+    {
+      start: '2022-05-07 01:30:00',
+      end: '2022-05-07 04:00:00',
+      title: 'SHSU Day At The Races',
+      summary: 'SHSU Alumni event @ Sam Houston Race Park',
+
+    },
+   
   ]);
 
   const eventClicked = (event) => {
